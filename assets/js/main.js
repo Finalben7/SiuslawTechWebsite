852f0b5656c5dc2022c6593e80057a4a38a107af
Finalben7 <kauffman4323@hotmail.com>
2026-07-30 15:33:35 -0700

    Added form confimration and logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,38 @@ document.documentElement.classList.add("js");
 document.addEventListener("DOMContentLoaded", function () {
   document.body.classList.add("loaded");
 });
+
+const contactForm = document.getElementById("contactForm");
+
+if (contactForm) {
+
+    contactForm.addEventListener("submit", async function (e) {
+
+        e.preventDefault();
+
+        const formData = new FormData(contactForm);
+
+        const response = await fetch(contactForm.action, {
+            method: "POST",
+            body: formData
+        });
+
+        const result = await response.json();
+
+        if (result.success) {
+
+            contactForm.innerHTML = `
+                <div class="form-success">
+                    <h2>Thank You!</h2>
+                    <p>
+                        Thank you for reaching out. Someone will get in touch shortly
+                        to help with your request.
+                    </p>
+                </div>
+            `;
+
+        } else {
+            alert(result.message);
+        }
+    });
+}
